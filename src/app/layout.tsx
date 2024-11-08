c9a1efea5032b555f34db093d8c135052d352e48
AayushMandora <aayushmandora76@gmail.com>
2024-11-08 13:14:38 +0530

    Fixed Deployment Errors.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import { Sidebar } from "@/components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { Footer } from "@/components/Footer";
 
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+// const inter = Inter({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
 
 export const metadata: Metadata = {
   title: "Aafi Malek - Developer",
@@ -25,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={twMerge(
-          inter.className,
           "flex antialiased h-screen overflow-hidden bg-black"
         )}
       >
