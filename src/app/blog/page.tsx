1e01079750c409f64da2bab32f790d8d61ae58cf
AayushMandora <aayushmandora76@gmail.com>
2024-11-08 12:28:50 +0530

    Modify Blog page as Coming Soon.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,9 +8,9 @@ import { Blogs } from "@/components/Blogs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blogs | John Doe",
+  title: "Blogs | Aafi Malek",
   description:
-    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+    "Aafi Malek is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
 };
 
 export default async function Blog() {
@@ -19,13 +19,10 @@ export default async function Blog() {
 
   return (
     <Container>
-      <span className="text-4xl">📝</span>
-      <Heading className="font-black pb-4">I write about technology</Heading>
-      <Paragraph className="pb-10">
-        Ever since <Highlight> I was a kid</Highlight>, I&apos;ve been
-        fascinated by technology.
-      </Paragraph>
-      <Blogs blogs={data} />
+      <div className="h-[65vh] flex flex-col justify-center items-center">
+        <span className="text-4xl">📝</span>
+        <Heading className="font-black pb-4">Coming Soon....</Heading>
+      </div>
     </Container>
   );
 }
